17c944aa988b1dce8822dbd0c85e7ab46f50f144
Gabriela Amadori <gabyenbrasil3@gmail.com>
2020-03-29 13:09:03 -0300

    Saludo

---
+++ Printer.java
@@ -0,0 +1,6 @@
+public class Printer{
+
+    public void print(String text){
+        System.out.println(text);
+    }
+}
---
+++ Start.java
@@ -0,0 +1,7 @@
+public class Start {
+    public static void main(String[] args){
+        Printer printer = new Printer();
+        String saludo = "Olá mundo";
+        printer.print(saludo);
+    }
+}
